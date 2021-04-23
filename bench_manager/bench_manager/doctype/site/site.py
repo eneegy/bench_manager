ff9b1209e7531d38981a7fbd736607fe21f0be50
thanhdao <thanhdn1989@gmail.com>
2021-04-23 12:14:19 +0700

    allow new site access db when create new with erpnext installed

--- bench_manager/bench_manager/doctype/site/site.py
+++ bench_manager/bench_manager/doctype/site/site.py
@@ -212,7 +212,7 @@ def create_site(site_name, install_erpnext, mysql_password, admin_password, key)
 			app_list = f.read()
 		if 'erpnext' not in app_list:
 			commands.append("bench get-app erpnext")
-		commands.append("bench --site {site_name} install-app erpnext".format(site_name=site_name))
+		commands.append("bench --site {site_name} install-app erpnext --no-mariadb-socket".format(site_name=site_name))
 	frappe.enqueue('bench_manager.bench_manager.utils.run_command',
 		commands=commands,
 		doctype="Bench Settings",
